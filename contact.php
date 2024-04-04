0ed9ad44f165247d09190581fe5c43f6cbf21cab
Ani <a.usanetashvili1@gmail.com>
2024-04-05 02:05:58 +0400

    Add php code for repeated html code

---
+++ contact.php
@@ -0,0 +1,49 @@
+<?php
+
+$projects = [
+    [
+        'image' => 'assets/image1.svg',
+        'title' => 'Project1',
+        'info' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+         Suspendisse bibendum.',
+        'info2' => 'Nunc non posuere consectetur, justo 
+         erat semper enim, non hendrerit dui odio id enim.',
+    ],
+    [
+        'image' => 'assets/image2.svg',
+        'title' => 'Project2',
+        'info' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+         Suspendisse bibendum.',
+        'info2' => 'Nunc non posuere consectetur, justo 
+         erat semper enim, non hendrerit dui odio id enim.',
+    ],
+    [
+        'image' => 'assets/image3.svg',
+        'title' => 'Project3',
+        'info' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+         Suspendisse bibendum.',
+        'info2' => 'Nunc non posuere consectetur, justo 
+         erat semper enim, non hendrerit dui odio id enim.',
+    ],
+];
+
+$posts = [
+    [
+        'image' => 'assets/card-image1.jfif',
+        'title' => 'Lorem ipsum dolor',
+        'date' => 'April 24, 2022 - 5 min read',
+        'info' => 'Nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.',
+    ],
+    [
+        'image' => 'assets/card-image2.jfif',
+        'title' => 'Lorem ipsum dolor',
+        'date' => 'April 03, 2022 - 4 min read',
+        'info' => 'Nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.',
+    ],
+    [
+        'image' => 'assets/card-image3.jfif',
+        'title' => 'Lorem ipsum dolor',
+        'date' => 'March 12, 2022 - 6 min read',
+        'info' => 'Nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.',
+    ],
+];
---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./style.css">
+    <?php include 'contact.php'; ?>
+</head>
+<body>
+    <header>
+        <div class="header">
+            <div>
+                <img class="logo" src="assets/logo.svg" alt="logo">
+            </div>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="">
+                                Blog
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Newsletter
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Twitter
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <section>
+        <div class = "main-container">
+            <div>
+                <h1>
+                    Recent <span>Projects</span>
+                </h1>
+            </div>
+                
+            <div class="large-cards">
+
+                <?php 
+                
+                    foreach($projects as $project){
+
+                        echo '  <div class="container2">
+                                    <div class="single-large-card">
+                                        <div class = "image-div">
+                                            <img class = "large-card-image" src="' . $project['image'] . '" alt="card-image">
+                                        </div>
+                                        
+                                        <div class="large-cards-text">
+                                            <h2> ' . $project['title'] . ' </h2>
+                                            <p class="paragraph"> ' . $project['info'] . ' </p>
+                                            <p class="paragraph"> ' . $project['info2'] . ' </p>
+                                        </div>
+                                    </div>
+                                </div>';
+
+                    }
+
+                ?>
+                
+            </div>
+
+            <h1>Latest <span>Posts</span></h2>
+            <div class="all-cards">                
+                <?php
+                    foreach($posts as $post){
+                        echo '<div class = "single-card">
+                                <div class = "cards-image">
+                                    <img class = "image" src="' . $post['image'] . '" alt="card-image">
+                                </div>
+                                <div class = "cards-text">
+                                    <h4> ' . $post['title'] . ' </h4>
+                                    <h5> ' . $post['date'] . ' </h5>
+                                    <p class = "inside-card-text"> ' . $post['info'] . ' </p>
+                                </div>
+                            </div>';
+                    }
+                ?>
+
+            </div>    
+        </div>        
+    </section>
+    <hr>
+    <footer>
+        <div class="footer">
+            <div>
+                <p class="copyright">
+                    © Copyright 2022  All rights reserved
+                </p>
+            </div>
+            <div class = "social-icons">
+                <a class="icon" href="#">
+                    <img class = "icon-image" src="assets/Frame1.svg" alt="icon">
+                </a>
+                <a href="#">
+                    <img class = "icon-image" src="assets/Frame2.svg" alt="icon">
+                </a>
+                <a href="#">
+                    <img class = "icon-image" src="assets/Frame3.svg" alt="icon">
+                </a>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
